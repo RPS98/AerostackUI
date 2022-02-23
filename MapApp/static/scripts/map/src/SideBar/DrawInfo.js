62763b0a2f7f16017061d76cb6efc73db3523cf0
RPS98 <rafa.perez.s98@gmail.com>
2022-02-23 14:59:12 +0100

    DrawInfo - v0

--- MapApp/static/scripts/map/src/SideBar/DrawInfo.js
+++ MapApp/static/scripts/map/src/SideBar/DrawInfo.js
@@ -1,7 +1,25 @@
 class DrawInfo
 {
-    constructor()
-    {
+    constructor() {
+        M.UAV_MANAGER.addInfoAddCallback(this._newUavCallback.bind(this));
+        this.uavSList = new SmartList();
+        this.htmlId = 'sideBar-right-UAVInfo-content';
+    }
 
+    _newUavCallback(callback, uavId) {
+        let uavList = M.UAV_MANAGER.getUavList();
+
+        if (this.uavSList.indexOf(uavId) == -1) {
+            this._addUav(uavId);
+        }
+    }
+
+    _addUav(uavId) {
+        let uavInfoHtml = HTMLUtils.addDict('div', `${this.htmlId}-UAV-${uavId}`, { 'class': 'btn btn-primary' }, 'Set Height (m)');
+        HTMLUtils.addToExistingElement(`${this.htmlId}`, [uavInfoHtml]);
+    }
+
+    _addUavInfo(uavId, infoDict) {
+        let parent = `${this.htmlId}-UAV-${uavId}`
     }
 }
